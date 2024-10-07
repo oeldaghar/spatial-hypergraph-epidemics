5242e9ed292b7458cae372af3366852c3b6cc888
Omar Sameh Eldaghar <oeldagha@unitary.cs.purdue.edu>
2024-10-07 15:15:00 -0400

    cleaned code for figures 4+5 and updated figures

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hypergraph-epidemic-testing
+
+## TODO
+1. update figures 
+2. combine david and yu data/code
+3. clean up files/source code
+4. generate tarred version of data and remove raw data files
--- code/analysis.jl
+++ code/analysis.jl
@@ -5,6 +5,7 @@ using CairoMakie
 using Statistics
 using ProgressMeter
 
+####### INDIVIDUAL PLOTS 
 function make_banded_plot(data,title="")
     # Make plotting data 
     f = Figure()
@@ -24,222 +25,158 @@ function make_banded_plot(data,title="")
     return f,ax 
 end
 
-fnames = filter!(x->endswith(x,".txt"),readdir("data/hypergraphs/"))
-fnames = get_fnames(get_gname(fnames[end]))
-filter!(x->endswith(x,".txt"),fnames)
-gnames = last.(split.(fnames,'/'))
-
-#plot one - make individual plot for each data set without comparing
-fnames = readdir("data/output/sirs/")
-filter!(x->endswith(x,".txt"),fnames)
-@showprogress for fname in fnames 
-    data = readdlm(joinpath("data/output/sirs/$fname"))
-    parts = split(fname,'%')
-    f,ax = make_banded_plot(data,"$(parts[1])\n$(parts[2])")
-    plot_file_name = fname[1:end-4]
-    plot_file_name="$plot_file_name%banded.png"
-    save("data/output/figures/$plot_file_name", f)
-end
-
-#zoom in on alpha = 2 to highlight hypergraph effects 
-fnames = readdir("data/output/sirs/")
-filter!(x->endswith(x,".txt"),fnames)
-filter!(x->occursin("-50000-",x),fnames)
-parts = map(x->split(x,"-"),fnames)
-filtered_fnames = filter(x->parse(Float64,split(x,"-")[5])==2.0,fnames)
-
-
-
-f = Figure(resolution=(1400,400))
-ax1 = Axis(f[1,1], 
-        xlabel="Time",
-        ylabel="Total Infections")
-ax2 = Axis(f[1,2], 
-            xlabel="Time",
-            ylabel="Total Infections")
-ax3 = Axis(f[1,3], 
-        xlabel="Time",
-        ylabel="Total Infections")
-linkaxes!(ax1,ax2,ax3)
-
-fname = filtered_fnames[1]
-data = readdlm(joinpath("data/output/sirs/$fname"))
-parts = split(fname,'%')
-
-hyper_avgs = mapslices(x->mean(x),data,dims=1)
-hyper_ylow = mapslices(x->quantile(x,0),data,dims=1)
-hyper_yhigh = mapslices(x->quantile(x,1),data,dims=1)
-hyper_avgs,hyper_ylow,hyper_yhigh = vec(hyper_avgs),vec(hyper_ylow),vec(hyper_yhigh)
-#render 
-lines!(ax1, hyper_avgs,color=(:blue,1.0))
-band!(ax1, 1:lastindex(hyper_avgs), hyper_ylow, hyper_yhigh, color=(:blue, 0.25))    
-
-#second one 
-fname = filtered_fnames[2]
-data = readdlm(joinpath("data/output/sirs/$fname"))
-parts = split(fname,'%')
-
-hyper_avgs = mapslices(x->mean(x),data,dims=1)
-hyper_ylow = mapslices(x->quantile(x,0),data,dims=1)
-hyper_yhigh = mapslices(x->quantile(x,1),data,dims=1)
-hyper_avgs,hyper_ylow,hyper_yhigh = vec(hyper_avgs),vec(hyper_ylow),vec(hyper_yhigh)
-#render 
-lines!(ax2, hyper_avgs,color=(:blue,1.0))
-band!(ax2, 1:lastindex(hyper_avgs), hyper_ylow, hyper_yhigh, color=(:blue, 0.25))    
-
-#third one 
-fname = filtered_fnames[3]
-data = readdlm(joinpath("data/output/sirs/$fname"))
-parts = split(fname,'%')
-
-hyper_avgs = mapslices(x->mean(x),data,dims=1)
-hyper_ylow = mapslices(x->quantile(x,0),data,dims=1)
-hyper_yhigh = mapslices(x->quantile(x,1),data,dims=1)
-hyper_avgs,hyper_ylow,hyper_yhigh = vec(hyper_avgs),vec(hyper_ylow),vec(hyper_yhigh)
-#render 
-lines!(ax3, hyper_avgs,color=(:blue,1.0))
-band!(ax3, 1:lastindex(hyper_avgs), hyper_ylow, hyper_yhigh, color=(:blue, 0.25))    
-CairoMakie.xlims!(1,620)
-f
-filename = "data/output/figures/final/time-average-diffuison-50000-alpha-2.pdf"
-CairoMakie.save(filename,f)
-# Make plotting data 
-
-###########
-
-for (i,fname) in enumerate(filtered_fnames)
-    data = readdlm(joinpath("data/output/sirs/$fname"))
-    parts = split(fname,'%')
-    f,ax = make_banded_plot(data,"$(parts[1])\n$(parts[2])")
-end
-
-
-
-
-
-#pick specific graph as example 
-#linear
-#sqrt 
-#squared 
-
-fnames
-#plot two - moving alpha 
-
-
-#plot three - vary normalization term 
-
-# xs = #discrete bins 
-# ys = #data 
-# boxplot(xs, ys)
-
-function boxplot_alphas_normalization()
+function make_individual_figures()
     fnames = readdir("data/output/sirs/")
-    filter!(x->occursin("-50000-",x),fnames)
-    figures = []
-    for normalization in ["linear","squared","sqrt"]
-        filtered_fnames = filter(x->occursin("-$normalization",x),fnames)
-        alphas = map(x->x[5],split.(first.(split.(filtered_fnames,'%')),'-'))
-        p = sortperm(alphas,by=x->parse(Float64,x))
-        filtered_fnames = filtered_fnames[p]
-        alphas = alphas[p]
-        
-        tmp = split.(first.(split.(filtered_fnames,'%')),"-")
-        gname = unique(map(x->join(x[1:4],"-"),tmp))
-
-        f = Figure()
-        ax = Axis(f[1,1],
-                xlabel="Alpha - value of hypergraph interpolate",
-                ylabel="trailing average infections",
-                title="$gname\nNormalization=$normalization")
-        xs = []
-        ys = []
-        for (ind,fname) in enumerate(filtered_fnames)
-            data = readdlm(joinpath("data/output/sirs/$fname"))
-            tmp = data[end-1000:end]
-            ys = vcat(ys,tmp)
-            xs = vcat(xs,[parse(Float64,alphas[ind]) for i=1:lastindex(tmp)])
-        end
-        f = CairoMakie.boxplot!(ax,Vector{Float64}(xs),Vector{Float64}(ys),width=1/15)
-        display(f)
-        push!(figures,f)
+    filter!(x->endswith(x,".txt"),fnames)
+    @showprogress for fname in fnames 
+        data = readdlm(joinpath("data/output/sirs/$fname"))
+        parts = split(fname,'%')
+        f,ax = make_banded_plot(data,"$(parts[1])\n$(parts[2])")
+        plot_file_name = fname[1:end-4]
+        plot_file_name="$plot_file_name%banded.png"
+        save("data/output/figures/$plot_file_name", f)
     end
-    return figures 
 end
+make_individual_figures()
 
+####### FIGURE 4 
+function figure4_hyperedge_sirs_difference()
+    #plotting params 
+    titlesize = 25
+    
+    xlabel_size = 25
+    xtick_label_size = 20
 
-figures = boxplot_alphas_normalization()
-display(figures[2])
+    ylabel_size = 25
+    ytick_label_size = 20
 
+    #load files 
+    fnames = readdir("data/output/sirs/")
+    filter!(x->endswith(x,".txt"),fnames)
+    #filter to the large graphs  
+    filter!(x->occursin("-50000-",x),fnames)
+    parts = map(x->split(x,"-"),fnames)
+    #zoom in on alpha=2.0
+    filtered_fnames = filter(x->parse(Float64,split(x,"-")[5])==2.0,fnames)
 
+    f = Figure(resolution=(1400,400))
+    ax1 = Axis(f[1,1], 
+            title= "g(m)=m",
+            xlabel="Time",
+            ylabel="Total Infections",
+            titlesize = titlesize,
+            yticklabelsize = ytick_label_size,
+            xticklabelsize = xtick_label_size,
+            ylabelsize=ylabel_size,
+            xlabelsize=xlabel_size)
+    hidespines!(ax1)
+    ax2 = Axis(f[1,2], 
+                title= "g(m)=sqrt(m)",
+                xlabel="Time",
+                titlesize = titlesize,
+                yticklabelsize = ytick_label_size,
+                xticklabelsize = xtick_label_size,
+                xlabelsize=xlabel_size,
+                yticklabelsvisible=false,
+                yticksvisible = false)
+    hidespines!(ax2)
+    ax3 = Axis(f[1,3], 
+            title= "g(m)=m²",
+            xlabel="Time",
+            titlesize = titlesize,
+            yticklabelsize = ytick_label_size,
+            xticklabelsize = xtick_label_size,
+            xlabelsize=xlabel_size,
+            yticklabelsvisible=false,
+            yticksvisible = false)
+    hidespines!(ax3)
+    linkaxes!(ax1,ax2,ax3)
 
-fnames = readdir("data/output/sirs/")
-filter!(x->endswith(x,".txt"),fnames)
-normalization = "squared"
-filtered_fnames = filter(x->occursin("-$normalization",x),fnames)
-filtered_fnames = filter(x->occursin("-50000-",x),filtered_fnames)
-alphas = map(x->x[5],split.(first.(split.(filtered_fnames,'%')),'-'))
-p = sortperm(alphas,by=x->parse(Float64,x))
-filtered_fnames = filtered_fnames[p]
-alphas = alphas[p]
-
-tmp = split.(first.(split.(filtered_fnames,'%')),"-")
-gname = unique(map(x->join(x[1:4],"-"),tmp))[1]
-
-f = Figure()
-ax = Axis(f[1,1],
-        xlabel="Alpha - value of hypergraph interpolate",
-        ylabel="trailing infections",
-        title="$gname\nNormalization=$normalization")
-xs = Vector{Float64}()
-ys = Vector{Float64}()
-for (ind,fname) in enumerate(filtered_fnames)
+    fname = filtered_fnames[1]
     data = readdlm(joinpath("data/output/sirs/$fname"))
-    tmp = vec(data[1:end,end-1000:end])
-    ys = vcat(ys,tmp)
-    xs = vcat(xs,[parse(Float64,alphas[ind]) for i=1:lastindex(tmp)])
-end
-CairoMakie.boxplot!(ax,xs,ys,width=1/15)
-# ylims!(ax,100,800)
-f
+    parts = split(fname,'%')
 
+    hyper_avgs = mapslices(x->mean(x),data,dims=1)
+    hyper_ylow = mapslices(x->quantile(x,0),data,dims=1)
+    hyper_yhigh = mapslices(x->quantile(x,1),data,dims=1)
+    hyper_avgs,hyper_ylow,hyper_yhigh = vec(hyper_avgs),vec(hyper_ylow),vec(hyper_yhigh)
+    #render 
+    lines!(ax1, hyper_avgs,color=(:blue,1.0))
+    band!(ax1, 1:lastindex(hyper_avgs), hyper_ylow, hyper_yhigh, color=(:blue, 0.25))    
 
-f = Figure()
-ax = Axis(f[1,1],
-        xlabel="Alpha - value of hypergraph interpolate",
-        ylabel="trailing infections",
-        title="$gname\nNormalization=$normalization")
-xs = Vector{Float64}()
-ys = Vector{Float64}()
-for (ind,fname) in enumerate(filtered_fnames)
+    #second one 
+    fname = filtered_fnames[2]
     data = readdlm(joinpath("data/output/sirs/$fname"))
-    tmp = data[1:end,end-1000:end]
-    ys = push!(ys,mean(tmp))
-    xs = push!(xs,parse(Float64,alphas[ind]))
+    parts = split(fname,'%')
+
+    hyper_avgs = mapslices(x->mean(x),data,dims=1)
+    hyper_ylow = mapslices(x->quantile(x,0),data,dims=1)
+    hyper_yhigh = mapslices(x->quantile(x,1),data,dims=1)
+    hyper_avgs,hyper_ylow,hyper_yhigh = vec(hyper_avgs),vec(hyper_ylow),vec(hyper_yhigh)
+    #render 
+    lines!(ax2, hyper_avgs,color=(:blue,1.0))
+    band!(ax2, 1:lastindex(hyper_avgs), hyper_ylow, hyper_yhigh, color=(:blue, 0.25))    
+
+    #third one 
+    fname = filtered_fnames[3]
+    data = readdlm(joinpath("data/output/sirs/$fname"))
+    parts = split(fname,'%')
+
+    hyper_avgs = mapslices(x->mean(x),data,dims=1)
+    hyper_ylow = mapslices(x->quantile(x,0),data,dims=1)
+    hyper_yhigh = mapslices(x->quantile(x,1),data,dims=1)
+    hyper_avgs,hyper_ylow,hyper_yhigh = vec(hyper_avgs),vec(hyper_ylow),vec(hyper_yhigh)
+    #render 
+    lines!(ax3, hyper_avgs,color=(:blue,1.0))
+    band!(ax3, 1:lastindex(hyper_avgs), hyper_ylow, hyper_yhigh, color=(:blue, 0.25))    
+    CairoMakie.xlims!(1,620)
+    f
+
+    filename = "data/output/figures/final/time-average-diffuison-50000-alpha-2.pdf"
+    CairoMakie.save(filename,f,px_per_unit=300)
+    return f 
 end
-lines!(ax,xs,ys)
-# ylims!(ax,100,500)
-f
+figure4_hyperedge_sirs_difference()
 
+####### FIGURE 5
+function figure5_trailing_infs_plot()
+    #plotting params 
+    titlesize = 25
+    
+    xlabel_size = 25
+    xtick_label_size = 20
 
-fnames = readdir("data/output/sirs/")
-filter!(x->endswith(x,".txt"),fnames)
+    ylabel_size = 25
+    ytick_label_size = 20
 
-
-##another one without mixing info across trajectories
-function make_trailing_infs_plot(test::Bool=false)
+    #file names 
     fnames = readdir("data/output/sirs/")
     filter!(x->endswith(x,".txt"),fnames)
 
     final_fig = Figure(resolution=(1400,400))
-    figures = []
+
+    titles = Dict()
+    titles["linear"] = "g(m)=m"
+    titles["sqrt"] = "g(m)=sqrt(m)"
+    titles["squared"] = "g(m)=m²"
+
+    gname = ""
+
     for (ind,normalization) in enumerate(["linear","sqrt","squared"])
+        
+        yinfo_bool = ind==1
         ax = Axis(final_fig[1,ind],
             xlabel="α",
-            ylabel="Average Trailing Infections",
-            title="",
-            titlesize=24,
-            xlabelsize=20,
-            ylabelsize=20)
+            ylabel= yinfo_bool ? "Trailing Infections" : "",
+            title=titles[normalization],
+            titlesize = titlesize,
+            yticklabelsize = ytick_label_size,
+            xticklabelsize = xtick_label_size,
+            ylabelsize=ylabel_size,
+            xlabelsize=xlabel_size,
+            yticklabelsvisible = yinfo_bool ? true : false,
+            yticksvisible = yinfo_bool ? true : false)
+        hidespines!(ax)
         
         #handle filenames and parameters 
         filtered_fnames = filter(x->occursin("-$normalization",x),fnames)
@@ -270,85 +207,8 @@ function make_trailing_infs_plot(test::Bool=false)
         band!(ax, parse.(Float64,alphas), ylow, yhigh, color=(:blue, 0.25))
         CairoMakie.ylims!(2500,4500)
     end
+    fname = "data/output/figures/final/trailing-infections-normalization-comparison.pdf"
+    CairoMakie.save(fname,final_fig,px_per_unit=300)
     return final_fig
 end
-
-
-figs = make_trailing_infs_plot(false)
-
-figs
-CairoMakie.save("data/output/figures/final/$gname-normalization-comparison.pdf",figs)
-CairoMakie.save("data/output/figures/final/$gname-normalization-comparison.png",figs,px_per_unit = 300)
-
-
-figs[1]
-figs[2]
-figs[3]
-
-
-
-## full banded simulation 
-
-
-figs = make_infs_plot()
-figs
-
-fnames = readdir("data/output/sirs/")
-filter!(x->endswith(x,".txt"),fnames)
-
-figures = []
-for normalization in ["linear","squared","sqrt"]
-    filtered_fnames = filter(x->occursin("-$normalization",x),fnames)
-    filter!(x->occursin(x,"-50000-"),filtered_fnames)
-    
-    alphas = map(x->x[5],split.(first.(split.(filtered_fnames,'%')),'-'))
-    p = sortperm(alphas,by=x->parse(Float64,x))
-    filtered_fnames = filtered_fnames[p]
-    alphas = alphas[p]
-    
-    tmp = split.(first.(split.(filtered_fnames,'%')),"-")
-    gname = unique(map(x->join(x[1:4],"-"),tmp))
-
-    f = Figure()
-    ax = Axis(f[1,1],
-            xlabel="Alpha - value of hypergraph interpolate",
-            ylabel="trailing average infections",
-            title="$gname\nNormalization=$normalization")
-    xs = Vector{Float64}()
-    ys = Vector{Float64}()
-    for (ind,fname) in enumerate(filtered_fnames)
-        data = readdlm(joinpath("data/output/sirs/$fname"))
-        tmp = data[end-1000:end]
-        ys = vcat(ys,tmp)
-        xs = vcat(xs,[parse(Float64,alphas[ind]) for i=1:lastindex(tmp)])
-    end
-    f = CairoMakie.boxplot!(ax,xs,ys,width=1/25)
-    push!(figures,f)
-end
-
-
-
-
-
-
-using Plots
-
-# Sample data
-x = 1:10
-y_mean = rand(10) * 10  # Mean values
-y_lower = y_mean .- 2   # Lower bound (mean - 2)
-y_upper = y_mean .+ 2   # Upper bound (mean + 2)
-
-# Create a band plot
-Plots.band(x, y_lower, y_upper, label="Confidence Interval", alpha=0.3, color=:blue)
-
-# Optional: Add a line in the middle for the mean
-plot!(x, y_mean, label="Mean", color=:black, linewidth=2)
-
-# Add titles and labels
-title!("Band Plot Example")
-xlabel!("X-axis")
-ylabel!("Y-axis")
-
-# Display the plot
-display(plot!)
+figure5_trailing_infs_plot()
